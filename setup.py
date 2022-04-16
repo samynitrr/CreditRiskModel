da66a2d0ccbf7a2471b71a7e953f2a01ac3a4cab
samynitrr <sameershekhar7@gmail.com>
2022-04-16 09:57:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a Project to build and deploy a Credit Risk Model that outputs the probabilities of a loan application going bad as a requirement for a technical assessment.',
+    author='Sameer Shekhar',
+    license='MIT',
+)
